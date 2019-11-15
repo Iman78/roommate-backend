3a5a3e017e248e4ee0f9298c3774cae53c4e5ce1
Mamri imane <imamri@valueit.ma>
2019-11-15 23:39:05 +0100

    First commit

---
+++ src/test/java/com/ilisi/roommatebackend/RommateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilisi.roommatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RommateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
